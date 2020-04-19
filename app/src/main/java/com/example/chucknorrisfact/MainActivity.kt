397d3ebda5b1cacaf7bf251202ef0248303ea365
Ibrahima-Sy <syibra10@hotmail.fr>
2020-04-19 14:19:51 +0200

    Smart cast to JokeAdapter error

--- app/src/main/java/com/example/chucknorrisfact/MainActivity.kt
+++ app/src/main/java/com/example/chucknorrisfact/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.chucknorrisfact.JokeList.jokeList
 import io.reactivex.Scheduler
 import io.reactivex.Single
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 
@@ -18,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
+    private val compositeDisp: CompositeDisposable= CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +28,16 @@ class MainActivity : AppCompatActivity() {
         val jokeServiceF = JokeApiServiceFactory
 
         val thaJoke = jokeServiceF.idle1().giveMeAJoke().subscribeOn(Schedulers.io()).subscribeBy (
-            onError= { Log.d("ERROR",it.toString())},
+            onError= { Log.d("ERROR","empty")},
             onSuccess={Log.d("SUCCESS",it.value)}
 
         )
 
+        compositeDisp.add(thaJoke)
+
+        //We only need to clear the resource if the disposable has NOT been disposed
+        if (!compositeDisp.isDisposed())
+            compositeDisp.clear()
 
         viewManager = LinearLayoutManager(this)
         viewAdapter = JokeAdapter(jokeList)
